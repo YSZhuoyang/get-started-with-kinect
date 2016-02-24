74ee2634428eb0a818015b93503e81d613633204
Oscar <yuszy@outlook.com>
2016-02-25 00:56:04 +1100

    Added canvas for rendering color video

---
+++ KinectUnity/Assets/CanvasRenderer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanvasRenderer : MonoBehaviour
+{
+    private Canvas colorVideoFrame;
+
+	// Use this for initialization
+	void Start ()
+    {
+        colorVideoFrame = GetComponent<Canvas>();
+        //colorVideoFrame.pixelRect.
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+}
